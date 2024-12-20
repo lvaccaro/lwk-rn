ee6c15ee77cb4532bbf6a982ab69c43bd9d8cf20
Luca Vaccaro <me@lvaccaro.com>
2024-12-20 01:27:05 +0100

    feat: add Bip and update Signer interface
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- src/classes/Signer.ts
+++ src/classes/Signer.ts
@@ -2,6 +2,7 @@ import { NativeLoader } from './NativeLoader';
 import { Pset } from './Pset';
 import { Network } from '../lib/enums';
 import { Descriptor } from './Descriptor';
+import { Bip } from './Bip';
 
 export class Signer extends NativeLoader {
   id: string = '';
@@ -22,5 +23,21 @@ export class Signer extends NativeLoader {
   async wpkhSlip77Descriptor(): Promise<Descriptor> {
     let newId = await this._lwk.wpkhSlip77Descriptor(this.id);
     return new Descriptor().from(newId);
+  }
+
+  async createRandomSigner(network: Network): Promise<Signer> {
+    if (!Object.values(Network).includes(network)) {
+      throw `Invalid network passed. Allowed values are ${Object.values(Network)}`;
+    }
+    this.id = await this._lwk.createRandomSigner(network);
+    return this;
+  }
+
+  async keyoriginXpub(bip: Bip): Promise<string> {
+    return await this._lwk.keyoriginXpub(this.id, bip.id);
+  }
+
+  async mnemonic(): Promise<string> {
+    return await this._lwk.mnemonic(this.id);
   }
 }
