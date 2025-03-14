efa365057c5f29fb55766c9f73ed57aa65062656
Luca Vaccaro <me@lvaccaro.com>
2025-03-14 01:30:50 +0100

    build: add release-smaller profile on Cargo.toml
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- fetch_lwk.sh
+++ fetch_lwk.sh
@@ -22,3 +22,12 @@ else
    sed -i 's/name = "lwk_bindings"/name = "lwk"/g' lwk_bindings/Cargo.toml
 fi
 
+cat << EOF >> lwk_bindings/Cargo.toml
+[profile.release-smaller]
+inherits = "release"
+opt-level = 'z'      # Optimize for size.
+lto = true           # Enable Link Time Optimization
+codegen-units = 1    # Reduce number of codegen units to increase optimizations.
+panic = "abort"      # Abort on panic
+strip = true         # Partially strip symbols from binary
+EOF
